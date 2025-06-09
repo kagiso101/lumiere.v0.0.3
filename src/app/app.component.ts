7af1ab93713a2b213d82b41b0ce3947a699db45d
kagiso101 <hadebekagiso3@gmail.com>
2025-06-09 21:55:32 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -9,7 +8,6 @@ import { ReactiveFormsModule } from '@angular/forms';
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    HomeComponent,
     FooterComponent,
     HeaderComponent,
     ReactiveFormsModule
